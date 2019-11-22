726e2c821d8690a26aae1366a2f0d6aea69450fa
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-22 14:21:18 +0000

    Set up Webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'static' }
+    ])
+  ]
+};
