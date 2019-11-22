6a7b31aba15450795e14edf50aa20c4b4d6d2dfb
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-22 16:30:02 +0000

    Create Complex type

--- src/render.ts
+++
@@ -1,8 +0,0 @@
-export const render = ctx => {
-  const { canvas } = ctx;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(0, 0, 100, 100);
-};
---
+++ src/types/Complex.ts
@@ -0,0 +1,20 @@
+export interface ComplexType {
+  real: number;
+  imaginary: number;
+  add: (Complex) => ComplexType;
+  subtract: (Complex) => ComplexType;
+  map: (Function) => ComplexType;
+  square: () => ComplexType;
+}
+
+export const Complex = (real: number, imaginary: number) => ({
+  real,
+  imaginary,
+  add: (x: ComplexType) => Complex(real + x.real, imaginary + x.imaginary),
+  subtract: (x: ComplexType) => Complex(real - x.real, imaginary - x.imaginary),
+  map: (fn: Function) => fn(Complex(real, imaginary)),
+  square: () => {
+    console.warn('UNFINISHED');
+    return Complex(real, imaginary);
+  }
+});
