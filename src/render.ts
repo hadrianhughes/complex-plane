149993d94980c9fa2aad648df28bbf100d161101
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-25 16:37:23 +0000

    Draw axes

--- src/render.ts
+++ src/render.ts
@@ -1,16 +1,23 @@
 import { lens } from './utils';
 import { State } from './update';
 
+const AXIS_PADDING = 30;
+
 export const render = ctx => (state: State) => {
   const { canvas } = ctx;
   canvas.width = canvas.width;
 
-  ctx.fillStyle = 'white';
+  // Draw axes
+  ctx.strokeStyle = 'white';
 
-  state.numbers.forEach(n => {
-    ctx.arc(n.real, n.imaginary, 5, 0, 2 * Math.PI);
-    ctx.fill();
-  });
+  ctx.moveTo(canvas.width / 2, AXIS_PADDING);
+  ctx.lineTo(canvas.width / 2, canvas.height - AXIS_PADDING);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(AXIS_PADDING, canvas.height / 2);
+  ctx.lineTo(canvas.width - AXIS_PADDING, canvas.height / 2);
+  ctx.stroke();
 };
 
 export default render;
