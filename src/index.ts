c6a530cd12889e9c7dc834cb0961e1b598c1df6b
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-25 15:16:30 +0000

    Create canvas element and render basic square

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { listen, getMouseLocation, getClicksSince } from './input';
+import render from './render';
+
+const CANVAS_WIDTH = window.innerWidth;
+const CANVAS_HEIGHT = window.innerHeight;
+
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+document.body.appendChild(canvas);
+
+render(context)({});
 
 listen();
--- src/render.ts
+++ src/render.ts
@@ -1,2 +1,11 @@
+import { lens } from './utils';
+
 export const render = ctx => state => {
+  const { canvas } = ctx;
+  canvas.width = canvas.width;
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(10, 10, 50, 50);
 };
+
+export default render;
