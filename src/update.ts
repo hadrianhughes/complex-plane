6fe1f2da0ce64c8a09a9ce96aae6f09ceedb3bef
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-25 16:28:19 +0000

    Add zoom level restraints to state

--- src/update.ts
+++ src/update.ts
@@ -3,18 +3,22 @@ import { getMouseLocation, getClicksSince } from './input';
 
 export interface State {
   numbers: ComplexType[];
+  realInView: number;
+  imaginaryInView: number;
 }
 
 const update = (state: State): State => {
   if (state.numbers.length === 0) {
-    return { numbers: [ Complex(50, 100) ] };
+    return { ...state, numbers: [ Complex(50, 100) ] };
   }
 
   return state;
 };
 
-export const initialState = {
-  numbers: []
+export const initialState: State = {
+  numbers: [],
+  realInView: 10,
+  imaginaryInView: 10
 };
 
 export default update;
