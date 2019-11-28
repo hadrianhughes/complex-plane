e47b2fc54e00f412771bcb33f52b588f41efd061
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-28 16:59:33 +0000

    Enable adding click handler to buttons

--- src/render/ui.ts
+++ src/render/ui.ts
@@ -1,9 +1,19 @@
 import { State } from '../update';
 import config from '../config';
 
-export const renderToString = (state: State): string => `
-  <button id="btnSettings" class="ui-button">Settings</button>
-`;
+export const button = (text: string, onClick: (this: GlobalEventHandlers, ev: MouseEvent) => any): HTMLButtonElement => {
+  const el = document.createElement('button');
+  el.className = 'ui-button';
+  el.innerHTML = text;
+  el.onclick = onClick;
+  return el;
+};
+
+export const buildUI = (state: State): Array<HTMLElement> => {
+  const btnSettings = button('Settings', () => console.log('got clicked'));
+
+  return [btnSettings];
+};
 
 export const getUIContainer = (): HTMLElement => {
   const el = document.getElementById(config.UI_CONTAINER_ID);
@@ -16,15 +26,22 @@ export const getUIContainer = (): HTMLElement => {
   return newEl;
 };
 
-export const addToDOM = (html: string): void => {
-  const container = getUIContainer();
+export const empty = (element: HTMLElement): void => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
 
-  container.innerHTML = html;
+export const addToDOM = (elements: Array<HTMLElement>): void => {
+  const container = getUIContainer();
+  empty(container);
+
+  elements.forEach(e => container.appendChild(e));
 };
 
 export const render = (state: State): void => {
-  const html = renderToString(state);
-  addToDOM(html);
+  const ui = buildUI(state);
+  addToDOM(ui);
 };
 
 export default render;
