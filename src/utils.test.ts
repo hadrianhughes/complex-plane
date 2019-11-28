2c582b0aae3291d6b980bd40a6e7592d60d6bdcf
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-11-28 15:09:46 +0000

    Create diff functions

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,10 @@
-import { isNullish, lens } from './utils';
+import {
+  isNullish,
+  lens,
+  diffArray,
+  diffObject,
+  diff
+} from './utils';
 
 describe('isNullish function', () => {
   it('Should accept any type of value without throwing an error', () => {
@@ -22,7 +28,7 @@ describe('isNullish function', () => {
   });
 });
 
-describe('get function', () => {
+describe('lens function', () => {
   it('Should accept a path array and an object and return a Maybe containing the value at that path', () => {
     const obj = {
       foo: {
@@ -32,5 +38,80 @@ describe('get function', () => {
 
     expect(lens(['foo', 'bar'])(obj).extract()).toBe(obj.foo.bar);
     expect(lens(['foo', 'baz'])(obj).isNothing()).toBe(true)
+  });
+});
+
+describe('diffArray function', () => {
+  it('Should return false if two arrays are identical', () => {
+    const input1 = ['one', 'two', 'three'];
+    expect(diffArray(input1, input1)).toBe(false);
+
+    const input2 = [1, 2, 3];
+    expect(diffArray(input2, input2)).toBe(false);
+
+    const input3 = ['first', { foo: { bar: 'baz' } }];
+    expect(diffArray(input3, input3)).toBe(false);
+  });
+
+  it('Should return true if two arrays are not identical', () => {
+    const input1 = ['one', 'two', 'three'];
+    const input2 = ['two', 'three', 'one'];
+
+    expect(diffArray(input1, input2)).toBe(true);
+  });
+});
+
+describe('diffObject function', () => {
+  it('Should perform a deep comparison and return false only if the two objects are identical', () => {
+    const input = {
+      foo: 'bar',
+      bar: {
+        baz: [1, 2, 3]
+      }
+    };
+
+    expect(diffObject(input, input)).toBe(false);
+  });
+
+  it('Should return true if the two objects are not identical', () => {
+    const input1 = {
+      foo: 'bar',
+      bar: {
+        baz: [1, 2, 3]
+      }
+    };
+
+    const input2 = {
+      foo: 'bar',
+      bar: {
+        bas: [1, 2, 3]
+      }
+    };
+
+    expect(diffObject(input1, input2)).toBe(true);
+  });
+
+  it('Should return true if one of the objects contains additional properties', () => {
+    const input1 = { foo: 'bar' };
+    const input2 = { foo: 'bar', bar: 2 };
+
+    expect(diffObject(input1, input2)).toBe(true);
+  });
+});
+
+describe('diff function', () => {
+  const inputObj = { foo: 'bar', bar: 'baz' };
+  const inputArr = [1, 2, 3];
+
+  it('Should accept two arrays or objects and return false if they are identical', () => {
+    expect(diff(inputObj, inputObj)).toBe(false);
+  });
+
+  it('Should return true if they are not identical', () => {
+    expect(diff(inputObj, { something: 'else' })).toBe(true);
+  });
+
+  it('Should return true if the given arguments are not homogeneous', () => {
+    expect(diff(inputObj, inputArr)).toBe(true);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -10,3 +10,44 @@ export const lens = (path: string[]) => (obj: object) =>
 
     return undefined;
   }, obj));
+
+export const diffArray = (arr1: Array<any>, arr2: Array<any>): boolean => {
+  if (arr1.length !== arr2.length) return true;
+
+  for (let i = 0;i < arr1.length;i += 1) {
+    if (typeof arr1[i] !== typeof arr2[i]) return true;
+
+    if (typeof arr1[i] === 'object' && diff(arr1[i], arr2[i])) return true;
+
+    if (arr1[i] !== arr2[i]) return true;
+  }
+
+  return false;
+};
+
+export const diffObject = (obj1: object, obj2: object): boolean => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (diffArray(keys1, keys2)) return true;
+
+  for (let i = 0;i < keys1.length;i += 1) {
+    const key = keys1[i];
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    if (typeof val1 !== typeof val2) return true;
+
+    if (typeof val1 === 'object' && diff(val1, val2)) return true;
+
+    if (val1 !== val2) return true;
+  }
+
+  return false;
+};
+
+export const diff = (obj1, obj2): boolean => {
+  if (Array.isArray(obj1) !== Array.isArray(obj2)) return true;
+
+  return Array.isArray(obj1) ? diffArray(obj1, obj2) : diffObject(obj1, obj2);
+};
